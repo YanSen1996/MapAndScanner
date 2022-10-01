23cbd423622c82d2660f73bdae0a94d4725edd8e
YanSen <yanse@tcd.ie>
2022-10-01 19:19:35 +0100

    Scanning result showing activity

--- app/src/main/java/com/example/mapandscanner/ScannerResultActivity.java
+++ app/src/main/java/com/example/mapandscanner/ScannerResultActivity.java
@@ -2,16 +2,21 @@ package com.example.mapandscanner;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScannerResultActivity extends AppCompatActivity {
     protected String scanningResult;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         scanningResult = intent.getStringExtra("Scanner Result");
+        setContentView(R.layout.activity_after_scanning);
+        tv = findViewById(R.id.textView_Showing);
+        tv.setText(scanningResult);
         System.out.println(scanningResult);
     }
 }
